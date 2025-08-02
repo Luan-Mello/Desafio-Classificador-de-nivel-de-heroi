b5d0338ec647698adc3cf3454d086db14c6414e5
Luan-Mello <luaaan.mello@outlook.com>
2025-08-01 22:43:23 -0300

    Adicionado Lista de Missões

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
 let nome = "Koda";
-let xp = 9400;
+let xp = 8450;
+
+
+// Lista de missões concluídas com o XP ganho em cada uma
+let missoes = [300, 450, 1200, 2000]; 
+
+for (let i = 0; i < missoes.length; i++) {
+    xp += missoes[i]; 
+    console.log("Missão " + (i + 1) + ": ganhou " + missoes[i] + " XP.");
+}
+
+console.log(nome + " acumulou um total de " + xp + " de XP.");
+
 
 let nivel;
 
@@ -15,7 +27,7 @@ if (xp < 100) {
 }  else if (xp <= 8000) {
     nivel = "PLATINA";
 }  else if (xp <= 9000) {
-    nivel = "Ascedente";
+    nivel = "Ascendente";
 }  else if (xp <= 10000) {
     nivel = "IMORTAL";
 }  else {
